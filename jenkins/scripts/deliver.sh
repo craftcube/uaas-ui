7fdc38401badef7ada83bbf5210596f8fa5bbc26
Chen Ming <chenming1@cffex.com.cn>
2018-09-11 09:53:07 +0800

    add jenkins pipeline scripts

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -24,6 +24,6 @@ echo $! > .pidfile
 set +x
 
 echo 'Now...'
-echo 'Visit http://localhost:8080 to see your Node.js/React application in action.'
-echo '(This is why you specified the "args ''-p 8080:8080''" parameter when you'
+echo 'Visit http://localhost:3000 to see your Node.js/React application in action.'
+echo '(This is why you specified the "args ''-p 3000:8080''" parameter when you'
 echo 'created your initial Pipeline as a Jenkinsfile.)'
